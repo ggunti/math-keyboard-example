e02f3f9ec6d875451e905d1690b650fa2949861e
ShaMan123 <shacharnen@gmail.com>
2020-05-24 19:44:28 +0300

    ignore warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Require cycle:'])
 import CommentsPage from './Comments/Comments.page';
 
 export default function App() {
